baa2df3414eb1f3ee7180145ea1f4d51a227a038
rgablek <roman.gablek@iteratec.at>
2019-12-05 17:11:00 +0100

    aoc 2019

---
+++ 5-1/5-1.kt
@@ -0,0 +1,49 @@
+import java.io.File
+import java.io.InputStream
+
+enum class Operation(val value: Int, val parameters: Int){
+    ABORT(99, 0),
+    ADDITION(1, 3),
+    MULTIPLICATION(2, 3)
+}
+
+fun main(args: Array<String>) {
+    val fileName = System.getProperty("user.dir") + "\\input.txt"
+    val inputStream: InputStream = File(fileName).inputStream()
+
+    inputStream.bufferedReader().useLines { lines -> 
+        lines.forEach { 
+            var inputs: MutableList<Int> = it.split(',').map { x -> Integer.valueOf(x) }.toMutableList()
+            var i = 0
+            while (i < inputs.size) {
+                var value = Integer.valueOf(inputs.get(i))
+                if (value == Operation.ABORT.value) break
+
+                when (value) {
+                    Operation.ADDITION.value -> {
+                        calculate(inputs, i, Operation.ADDITION)
+                        i += Operation.ADDITION.parameters + 1
+                    }
+                    Operation.MULTIPLICATION.value -> {
+                        calculate(inputs, i, Operation.MULTIPLICATION)
+                        i += Operation.MULTIPLICATION.parameters + 1
+                    }
+                }
+            }
+
+            println(inputs.get(0))
+        } 
+    }
+}
+
+fun calculate(inputs: MutableList<Int>, i: Int, op: Operation) {
+    var parameters: List<Int> = getParameters(inputs, i, op)
+    when (op) {
+        Operation.ADDITION -> inputs[parameters.get(2)] = inputs.get(parameters.get(0)) + inputs.get(parameters.get(1))
+        Operation.MULTIPLICATION -> inputs[parameters.get(2)] = inputs.get(parameters.get(0)) * inputs.get(parameters.get(1))
+        Operation.ABORT -> return
+        
+    }
+}
+
+fun getParameters(inputs: List<Int>, i: Int, op: Operation) = inputs.filterIndexed { index, _ -> index in i+1..i+op.parameters }
